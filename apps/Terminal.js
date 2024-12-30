20515d412971c049a99bad5aae71d925a2dcca9b
Dennis Kane <dkane75@gmail.com>
2024-12-30 16:46:03 -0500

    I'm crushing your head!

--- apps/Terminal.js
+++ apps/Terminal.js
@@ -1,4 +1,7 @@
-
+/*Just had an issue with EOFs inside of scripts @LPIRHSKF. For some reason, it was
+assumed that EOFs should only go to the next commands in your immediate pipeline,
+and not to the scriptOut callback...
+*/
 let USE_ONDEVRELOAD = false;
 //let USE_DEVPARSER = false;
 let USE_DEVPARSER = true;
@@ -645,7 +648,10 @@ const Com = class {//«
 			};
 			this.ok=(mess)=>{
 				if (mess) this.suc(mess);
-				if (this.pipeTo) this.out(EOF);
+//log(this.name.toString(), this.pipeTo);
+//				if (this.pipeTo) {
+				this.out(EOF);
+//				}
 				Y(E_SUC);
 				this.killed = true;
 			};
@@ -761,7 +767,10 @@ log(num);
 		if (this.shell.cancelled) return;
 		const{term}=this;
 		let redir_lns = this.redirLines;
-		if (isEOF(val) || (!redir_lns && this.nextCom)){
+//log(val);
+//LPIRHSKF
+//		if (isEOF(val) || (!redir_lns && this.nextCom)){
+		if ((!redir_lns && this.nextCom)){
 			let next_com = this.nextCom;
 			if (next_com && next_com.pipeIn && !next_com.noPipe) {
 				next_com.pipeIn(val);
@@ -771,6 +780,7 @@ log(num);
 		//WLKUIYDP
 		if (redir_lns) {/*«*/
 		//KIUREUN
+			if (isEOF(val)) return;
 			if (val instanceof Uint8Array){
 				if (!redir_lns.length) {
 					redir_lns = val;
@@ -790,6 +800,7 @@ log(num);
 		if (this.scriptOut) {
 			return this.scriptOut(val);
 		}
+		if (isEOF(val)) return;
 		//Save to subLines and call scriptOut
 		if (this.subLines){
 			if (val instanceof Uint8Array) val = `Uint8Array(${val.length})`;
@@ -874,13 +885,18 @@ log(rv);
 this.shell.fatal("Non-numerical return value");
 }
 		if (!rv){
-			this.end(await this.shell.executeStatements2(conseqs[i], opts));
+			rv = await this.shell.executeStatements2(conseqs[i], opts)
+//			this.out(EOF);
+			this.end(rv);
 			return;
 		}
 	}
 	if (this.fallback){
-		return this.end(await this.shell.executeStatements2(this.fallback, opts));
+		rv = await this.shell.executeStatements2(this.fallback, opts)
+//		this.out(EOF);
+		return this.end(rv);
 	}
+//	this.out(EOF);
 	this.end(E_ERR);
 }//»
 
@@ -1461,7 +1477,7 @@ async run(){
 		this.no(`the prompt is too wide (have ${use_prompt.length}, max = ${term.w - 4})`);
 		return;
 	}
-	let ln = await term.read_line(use_prompt);
+	let ln = await term.readLine(use_prompt);
 	let vals = ln.trim().split(/ +/);
 	while (args.length){
 		let arg = args.shift();
@@ -2199,7 +2215,7 @@ async more(no_nl){/*«*/
 	let nl;
 	if (no_nl) nl="";
 	else nl="\n";
-	let rv = nl+(await this.terminal.read_line("> "));
+	let rv = nl+(await this.terminal.readLine("> "));
 //log(RV, `${rv}`);
 	this.source = this.source.concat(...rv);
 	this.length = this.source.length;
@@ -2876,310 +2892,6 @@ return await this.scanWord(null, this.env);
 
 //»
 
-//Runtime«
-
-/*«Notes
-
-12/25/24: Now that I am getting a development "mise en place" working
-internally in LOTW, I will be able to stop worrying over all of those
-little details, and start to think specifically about stuff like
-turning the ast from the new parser into a runtime object that is
-actually able to do "real world" execution.
-
-»*/
-
-//Imports«
-
-/*«
-const {globals} = LOTW;
-const{ShellMod}=globals;
-const{Shell}=ShellMod;
-const{Word}=ShellMod.seqClasses;
-const isWord=val=>{return val instanceof Word;};
-const{consoleLog, isStr, isFunc, log: _log, cwarn: _cwarn, cerr: _cerr}=LOTW.api.util;
-const log=(...args)=>{
-	consoleLog.nlog("LOG",...args);
-	_log(...args);
-} 
-const cwarn=(...args)=>{
-	consoleLog.nlog("WRN",...args);
-	_cwarn(...args);
-}
-const cerr=(...args)=>{
-	consoleLog.nlog("ERR",...args);
-	_cerr(...args);
-}
-»*/
-
-//const{Word}=ShellMod.seqClasses;
-const isWord=val=>{return val instanceof Word;};
-
-//»
-/*
-class Command{//«
-
-constructor(pref, name, suffix, cwd){//«
-	this.prefix=pref;
-	if (name) this.name=name.toString();
-	this.suffix=suffix;
-	this.assigns=[];
-	this.redirs=[];
-	this.cwd=cwd;
-}//»
-
-extractAssignments(){//«
-	if (!this.prefix) return;
-	for (let what of this.prefix){
-		if (isWord(what)){
-			this.assigns.push(what);
-		}
-	}
-}//»
-extractRedirects(){//«
-	if (this.prefix){
-		for (let what of this.prefix){
-			if (what.redir){
-				this.redirs.push(what);
-			}
-		}
-	}
-	if (this.suffix){
-		for (let what of this.suffix){
-			if (what.redir){
-				this.redirs.push(what);
-			}
-		}
-	}
-}//»
-get isPath(){return this.name.match(/\x2f/);}
-async setScript(){//«
-	let node = await this.name.toNode({cwd: this.cwd});
-	if (!node){
-		this.error = `file not found`;
-	}
-	else{
-		let text = await node.text;
-		if (!isStr(text)){
-			this.error = `no text was returned`;
-		}
-		else{
-			this.scriptText = text;
-		}
-	}
-}//»
-async setFunction(){//«
-
-let com = Shell.activeCommands[this.name];
-if (isStr(com)){//«
-//log(`Load lib: ${com} for command: ${this.name}`);
-	try{
-	    await ShellMod.util.importComs(com);
-	}catch(e){
-log("ERROR1");
-cerr(e);
-	    this.error = `command library: '${com}' could not be loaded`;
-	    return;
-	}
-	let gotcom = Shell.activeCommands[this.name];
-	if (!(gotcom instanceof Function)){
-log("ERROR2");
-cerr("GOTCOM", gotcom);
-		this.error = `'${this.name}' is invalid or missing in command library: '${com}'`;
-	    return;
-	}
-//log("LOAD OK!");
-	this.func = gotcom;
-}//»
-else if (isFunc(com)){//«
-	this.func = com;
-}//»
-else if (com){//«
-cerr(com);
-	this.error = `${this.name}: unknown entity returned (see console)`
-}//»
-else{//«
-	this.error = `${this.name}: command not found`;
-}//»
-
-}//»
-
-}//»
-class Pipeline{//«
-
-constructor(ast, bang, type){//«
-	this.ast=ast;
-	this.hasBang=bang;
-	if (type) this.type=type;
-}//»
-get isOr(){return this.type==="||";}
-get isAnd(){return this.type==="&&";}
-get isFirst(){return !this.type;}
-async extractCommands(cwd){//«
-	const{ast}=this;
-	let coms=[];
-	for (let obj of this.ast){
-		let o = obj.simple_command;
-		let com = new Command(o.prefix, o.word||o.name, o.suffix, cwd);
-		com.extractAssignments(); 
-		com.extractRedirects(); 
-		if (!com.name){
-
-		}
-		else if (com.isPath){
-			await com.setScript();
-		}
-		else{
-			await com.setFunction();
-		}
-		coms.push(com);
-	}
-}//»
-
-}//»
-class AndOr{//«
-
-constructor(ast, sep){//«
-	this.ast = ast;
-	this.sep = sep;
-}//»
-get isAsync(){return this.sep==="&";}
-async extractPipelines(cwd){//«
-	const{ast}=this;
-	let pipes=[];
-	let first = ast.shift();
-	pipes.push(new Pipeline(first.pipeline.pipe_sequence, first.bang));
-	for (let i=0; i < ast.length; i+=2){
-		pipes.push(new Pipeline(ast[i+1].pipeline.pipe_sequence, ast[i+1].bang, ast[i]));
-	}
-	for (let pipe of pipes){
-		await pipe.extractCommands(cwd);
-	}
-}//»
-
-}//»
-*/
-this.Runtime = class{
-
-constructor(ast, term, opts={}){//«
-
-	this.ast = ast;
-	this.term = term;
-	this.cwd = this.term.cwd;
-	this.opts = opts;
-
-}//»
-fatal(mess){throw new Error(mess);}
-
-async makeSimple(com){
-//This should be a bultin (JS) command or a script or No Com in the case of
-//assignments/redirects only, or an Error (not found, etc).
-
-}
-async makeIf(com){
-
-}
-
-async executePipeline(pipeobj){//«
-	const{pipeline, bang} = pipeobj;
-	const pipe_seq = pipeline.pipe_sequence;
-	//let rv;
-	let coms=[];
-	for (let com of pipe_seq){
-		if (com.simple_command){
-			coms.push(await this.makeSimple(com));
-		}
-		else if (com.if_clause){
-			coms.push(await this.makeIf(com));
-		}
-		else{
-cwarn("Here it is below");
-log(com);
-			this.fatal(`Unknown command found (see console)`);
-		}
-	}
-}//»
-async executeAndOr(andor, opts={}){
-//cwarn("EXECUTEANDOR!!!");
-//let first = andor.shift();
-//log("FIRST", first);
-let rv = await this.executePipeline(andor.shift());
-/*
-while(andor.length){
-let type = andor.shift();
-cwarn(`NEXT TYPE: ${type}`);
-let pipe = andor.shift();
-log(pipe);
-}
-*/
-}
-async executeAndOrs(){//«
-	let compcoms = this.ast.program.complete_commands;
-	let andors=[];
-	for (let compcom of compcoms){
-		let list = compcom.complete_command.list;
-		for (let i=0; i < list.length-1; i+=2){
-			let andor = list[i].andor;
-			let type = list[i+1];
-			if (type===";"){
-				await this.executeAndOr(andor);
-			}
-			else if (type==="&"){
-				await this.executeAndOr(andor, {background: true});
-			}
-			else{
-				this.fatal(`Invalid or missing andor type ${type}`);
-			}
-//cwarn("TYPE",list[i+1]);
-//jlog(list[i]);
-//			let andor = new AndOr(list[i].andor, list[i+1])
-//			await andor.extractPipelines(cwd);
-//			andors.push(andor);
-		}
-	}
-	return andors;
-}//»
-async execute(){//«
-
-let andors = await this.executeAndOrs();
-//log(andors);
-//nlog("AndOrs",andors)
-//nlog("COMS",Shell.activeCommands);
-
-}//»
-
-}
-
-/*«
-this.Runtime = class{
-
-constructor(ast, term, opts={}){
-	this.ast = ast;
-	this.term = term;
-//Here we can set policies about, for example whether we allow '&'-ended statements.
-	this.opts = opts;
-}
-
-async execute(){
-
-//Array brackets means it is a list of the next type
-//program->complete_commands[]->complete_command->list[]->andor[]->pipeline->pipe_sequence[]->command
-
-//Every list should have an even number of elements:
-//andor_1, sep_1 [ ,andor_2, sep_2 [, andor_3, sep_3 [,... [ ,andor_n, sep_n ] ] ] ]
-
-//The same is true for terms (which are the lists of andors of compound commands).
-
-//log("WHAT ME EXECUTE", this.ast, this.term, this.opts);
-cwarn("GIVE ME ANOTHER RUNTIME");
-log(this.ast);
-
-}
-
-}
-»*/
-
-//»
-
 //Dev Parser«
 
 const _DevParser = class {
@@ -3206,7 +2918,7 @@ constructor(code, opts={}) {//«
 	this.numToks = 0;
 	this.tokens = [];
 /*
-Since this is async (because we might need to get more lines from 'await terminal.read_line()'),
+Since this is async (because we might need to get more lines from 'await terminal.readLine()'),
 then we need to do 'await this.scanNextTok()' **outside** of the constructor, since there is
 NO async/await in constructors.
 */
@@ -3347,7 +3059,7 @@ getNextNonNewlinesTok(){//«
 }//»
 async getNonEmptyLineFromTerminal(){//«
 	let rv;
-	while ((rv = await this.terminal.read_line("> ")).match(/^[\x20\t]*(#.+)?$/)){}
+	while ((rv = await this.terminal.readLine("> ")).match(/^[\x20\t]*(#.+)?$/)){}
 	return rv;
 }//»
 async getMoreTokensFromTerminal(){//«
@@ -4425,7 +4137,7 @@ for (let i=0; i < coms.length; i++){
 			if (!coms[i+1]) {
 				if (opts.isInteractive){
 					let rv;
-					while ((rv = await opts.terminal.read_line("> ")).match(/^ *$/)) {
+					while ((rv = await opts.terminal.readLine("> ")).match(/^ *$/)) {
 						command_str+="\n";
 					}
 					return Parser.parse(command_str+"\n"+rv, opts);
@@ -4447,7 +4159,7 @@ for (let i=0; i < coms.length; i++){
 	else {
 		if (opts.isInteractive && !coms[i+1]){
 			let rv;
-			while ((rv = await opts.terminal.read_line("> ")).match(/^ *$/)) {
+			while ((rv = await opts.terminal.readLine("> ")).match(/^ *$/)) {
 				command_str+="\n";
 			}
 			return Parser.parse(command_str+"\n"+rv, opts);
@@ -4506,7 +4218,7 @@ constructor(code, opts={}) {//«
 	this.numToks = 0;
 	this.tokens = [];
 /*
-Since this is async (because we might need to get more lines from 'await terminal.read_line()'),
+Since this is async (because we might need to get more lines from 'await terminal.readLine()'),
 then we need to do 'await this.scanNextTok()' **outside** of the constructor, since there is
 NO async/await in constructors.
 */
@@ -6011,7 +5723,8 @@ async executeStatements2(statements, opts){//«
 
 async devexecute(command_str, opts){//«
 	const{term}=this;
-	let parser = new _DevParser(command_str.split(""), {terminal: term, isInteractive: false});
+//	let parser = new _DevParser(command_str.split(""), {terminal: term, isInteractive: false});
+	let parser = new _DevParser(command_str.split(""), opts);
 	try{
 
 		let errmess;
@@ -6453,6 +6166,7 @@ this.statdiv = statdiv;
 }//»
 
 //Execute«
+
 async execute(str, opts={}){//«
 
 	this.env['USER'] = globals.CURRENT_USER;
@@ -6481,7 +6195,7 @@ async execute(str, opts={}){//«
 	}//»
 
 //PLDYHJKU
-	await this.curShell.execute(str,{env, heredocScanner, isInteractive: true});
+	await this.curShell.devexecute(str,{env, heredocScanner, isInteractive: true, terminal: this});
 	this.responseEnd();
 
 	if (opts.noSave) return;
@@ -6508,6 +6222,7 @@ executeBackgroundCommand(s){//«
 	shell.execute(s,{env});
 
 }//»
+
 //»
 
 //Util«
@@ -8359,6 +8074,7 @@ responseEnd(opts={})  {//«
 //»
 response(out, opts={}){//«
 	const{actor}=this;
+	if (isEOF(out)) return;
 	if (!isStr(out)) this.Win._fatal(new Error("Non-string given to terminal.response"));
 
 	let {didFmt, colors, pretty, isErr, isSuc, isWrn, isInf, inBack} = opts;
